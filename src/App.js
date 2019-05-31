d6f3f0bb749d1dc1bd4578d5e1c08344c6e97f99
roglol <otikopotskhoraia@gmail.com>
2019-05-31 09:19:13 +0400

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
 import React from 'react';
-import logo from './logo.svg';
+import  {createStore} from 'redux';
+import {Provider} from 'react-redux';
+import {BrowserRouter, Route} from 'react-router-dom'
 import './App.css';
+
+import Esaa from './container/Esaa';
+const initialState = {
+  initialValue: 0
+}
+const reducer = (state=initialState, action) =>{
+  switch(action.type) {
+       case "ADD": {
+         let newState = {...state}
+         return {
+           initialValue:newState.initialValue + action.value 
+         }
+       }
+       case "MINUS": {
+        let newState = {...state}
+        return {
+          initialValue:newState.initialValue - action.value 
+        }
+      }
+  }
+}
+
+const store = createStore(reducer);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Provider store={store}>
+      <BrowserRouter>
+      <div className="App">
+        <Route path="/" component={Esaa} />
+        
     </div>
+      </BrowserRouter>
+
+    </Provider>
+   
   );
 }
 
